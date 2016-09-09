04d1efa0055d8a75144effebd89e1e233abc5099
matias <mati92.ss@gmail.com>
2016-09-09 18:38:31 -0300

    Use max limit allowed by Spotify.

--- app/actions/user.js
+++ app/actions/user.js
@@ -4,14 +4,14 @@ import ConstantFactory from '../utils/constantFactory';
 export const USER_PLAYLIST_CONSTANTS = ConstantFactory.buildAsyncFor('USER_PLAYLISTS');
 export const PLAYLIST_TRACKS_CONSTANTS = ConstantFactory.buildAsyncFor('PLAYLIST_TRACKS');
 
-export function fetchUserPlaylists(offset = 0, limit = 20){
+export function fetchUserPlaylists(offset = 0, limit = 50){
   return ActionFactory.buildSpotifyAction(USER_PLAYLIST_CONSTANTS, {
     apiCall: 'getUserPlaylists',
     args: [{offset, limit}]
   })
 }
 
-export function fetchPlaylistTracks(userId, playlistId, offset = 0, limit = 50){
+export function fetchPlaylistTracks(userId, playlistId, offset = 0, limit = 100){
   return ActionFactory.buildSpotifyAction(PLAYLIST_TRACKS_CONSTANTS, {
     apiCall: 'getPlaylistTracks',
     args: [userId, playlistId, {offset, limit}]
