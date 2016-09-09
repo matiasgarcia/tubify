a4b1c1be06d0ae34f2bd9848538fa0b7ec2d5fde
Matias Garcia <garcia.matias92@gmail.com>
2016-09-09 13:49:39 -0300

    Uppercase constant name.

--- app/actions/user.js
+++ app/actions/user.js
@@ -1,13 +1,13 @@
 import ActionFactory from '../utils/actionFactory';
 import ConstantFactory from '../utils/ConstantFactory';
 
-export const userPlaylistConstants = ConstantFactory.buildAsyncFor('USER_PLAYLISTS');
-export const playlistTracksConstants = ConstantFactory.buildAsyncFor('PLAYLIST_TRACKS');
+export const USER_PLAYLIST_CONSTANTS = ConstantFactory.buildAsyncFor('USER_PLAYLISTS');
+export const PLAYLIST_TRACKS_CONSTANTS = ConstantFactory.buildAsyncFor('PLAYLIST_TRACKS');
 
 export function fetchUserPlaylists(offset = 0, limit = 20){
-  return ActionFactory.buildSpotifyAction(userPlaylistConstants, 'getUserPlaylists', {offset, limit})
+  return ActionFactory.buildSpotifyAction(USER_PLAYLIST_CONSTANTS, 'getUserPlaylists', {offset, limit})
 }
 
 export function fetchPlaylistTracks(userId, playlistId, offset = 0, limit = 50){
-  return ActionFactory.buildSpotifyAction(playlistTracksConstants, 'getPlaylistTracks', userId, playlistId, {offset, limit});
+  return ActionFactory.buildSpotifyAction(PLAYLIST_TRACKS_CONSTANTS, 'getPlaylistTracks', userId, playlistId, {offset, limit});
 }
