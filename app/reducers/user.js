5529f4888d3c6128f34ecc9b31c2e1377c903506
Matias Garcia <garcia.matias92@gmail.com>
2016-09-09 15:50:58 -0300

    Refactor: Extract into  methods.

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -9,6 +9,49 @@ const initialState = {
   error: null
 };
 
+function playlistTracksSucccess(state, action){
+  let tracks = _.map(action.data.items, (item) => {
+    let track = item.track;
+    return {
+      name: track.name,
+      album: track.album.name,
+      artists: _.map(track.artists, (artist) => artist.name)
+    }
+  });
+  let updatedPlaylists = _.cloneDeep(state.playlists);
+  let index = _.findIndex(updatedPlaylists, (playlist) => playlist.id == action.data.playlistId);
+  let playlist = updatedPlaylists[index];
+  playlist.tracks = _.concat(playlist.tracks, tracks);
+  return Object.assign({}, state, {
+    playlists: updatedPlaylists,
+    isFetching: false,
+    error: null
+  });
+}
+
+function userPlaylistsSuccess(state, action){
+  let data = action.data;
+  let newPlaylists = _.map(data.items, (playlistData) => {
+    return {
+      id: playlistData.id,
+      name: playlistData.name,
+      userId: playlistData.owner.id,
+      totalTracksCount: playlistData.tracks.total,
+      tracks: [],
+      isPublic: playlistData.public
+    }
+  });
+  let updatedPlaylists = _.concat(state.playlists, newPlaylists);
+  let totalPlaylists = data.total;
+  return Object.assign({}, state, {
+    totalCount: totalPlaylists,
+    pendingCount: totalPlaylists - updatedPlaylists.length,
+    playlists: updatedPlaylists,
+    isFetching: false,
+    error: null
+  });
+}
+
 export default function playlists(state = initialState, action) {
   switch (action.type) {
     case USER_PLAYLIST_CONSTANTS.PENDING:
@@ -17,26 +60,7 @@ export default function playlists(state = initialState, action) {
         error: null
       });
     case USER_PLAYLIST_CONSTANTS.SUCCESS:
-      let data = action.data;
-      let newPlaylists = _.map(data.items, (playlistData) => {
-        return {
-          id: playlistData.id,
-          name: playlistData.name,
-          userId: playlistData.owner.id,
-          totalTracksCount: playlistData.tracks.total,
-          tracks: [],
-          isPublic: playlistData.public
-        }
-      });
-      let updatedPlaylists = _.concat(state.playlists, newPlaylists);
-      let totalPlaylists = data.total;
-      return Object.assign({}, state, {
-        totalCount: totalPlaylists,
-        pendingCount: totalPlaylists - updatedPlaylists.length,
-        playlists: updatedPlaylists,
-        isFetching: false,
-        error: null
-      });
+      return userPlaylistsSuccess(state, action);
     case USER_PLAYLIST_CONSTANTS.FAILURE:
       return Object.assign({}, state, {
         isFetching: false,
@@ -48,23 +72,7 @@ export default function playlists(state = initialState, action) {
         error: null
       });
     case PLAYLIST_TRACKS_CONSTANTS.SUCCESS:
-      let tracks = _.map(action.data.items, (item) => {
-        let track = item.track;
-        return {
-          name: track.name,
-          album: track.album.name,
-          artists: _.map(track.artists, (artist) => artist.name)
-        }
-      });
-      let updatedPlaylists = _.cloneDeep(state.playlists);
-      let index = _.findIndex(updatedPlaylists, (playlist) => playlist.id == action.data.playlistId);
-      let playlist = updatedPlaylists[index];
-      playlist.tracks = _.concat(playlist.tracks, tracks);
-      return Object.assign({}, state, {
-        playlists: updatedPlaylists,
-        isFetching: false,
-        error: null
-      });
+      return playlistTracksSucccess(state, action);
     case PLAYLIST_TRACKS_CONSTANTS.FAILURE:
       return Object.assign({}, state, {
         isFetching: false,
