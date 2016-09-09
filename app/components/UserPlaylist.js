5f27581568a2c49cdd199a2fdb8a631050792ca2
matias <mati92.ss@gmail.com>
2016-09-09 18:35:32 -0300

    Add tracks display.

--- app/components/UserPlaylist.js
+++ app/components/UserPlaylist.js
@@ -2,6 +2,26 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import _ from 'lodash';
 
+class Playlist extends Component {
+  constructor(props){
+    super(props);
+    this.loadTracks = this.loadTracks.bind(this);
+  }
+  loadTracks(e){
+    e.preventDefault();
+    this.props.fetchTracks(this.props.userId, this.props.id);
+  }
+  render(){
+    return (
+      <div>
+        <li>{this.props.name} ({this.props.totalTracksCount} tracks)</li> <a href="#" onClick={this.loadTracks}>Load Tracks</a>
+        <ul>
+          {_.map(this.props.tracks, (track) => <li key={track.id}>{track.name}</li>)}
+        </ul>
+      </div>
+    )
+  }
+}
 
 export default class UserPlaylist extends Component {
   static propTypes = {
@@ -18,7 +38,7 @@ export default class UserPlaylist extends Component {
     return (
       <div>
         <ul>
-          { _.map(this.props.playlistsData.playlists, (playlist) => <li key={playlist.id}>{playlist.name}</li>) }
+          { _.map(this.props.playlistsData.playlists, (playlist) => <Playlist {...playlist} fetchTracks={this.props.playlistActions.fetchPlaylistTracks}/>)}
         </ul>
       </div>
     );
