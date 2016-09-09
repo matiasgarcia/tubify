4cb905f0a0fce5893db95d29385107f34bd31865
Matias Garcia <garcia.matias92@gmail.com>
2016-09-08 23:15:22 -0300

    Rename module.

--- downloaders.js
+++ downloaders.js
@@ -1,11 +1,11 @@
 import { execFile } from 'child_process'
 
-export class Downloader {
-  constructor(downloadPath){
+export class YoutubeDownloader {
+  constructor({downloadPath}){
     this.options = { cwd: downloadPath };
   }
 
-  downloadFromYoutube(link){
+  download(link){
     return new Promise((resolve, reject) => {
       execFile('youtube-dl', ['-f', 'bestaudio[ext=m4a]/bestaudio', link], this.options, (error, stdout, stderr) => {
         if (error || stderr) reject(error, stderr);
