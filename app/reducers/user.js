255b4e46245da4a7a304ade9f2a7d66fb92a6ffa
Matias Garcia <garcia.matias92@gmail.com>
2016-09-09 15:00:39 -0300

    Add PlaylistTracks reducers.

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { USER_PLAYLIST_CONSTANTS } from '../actions/user';
+import { USER_PLAYLIST_CONSTANTS, PLAYLIST_TRACKS_CONSTANTS } from '../actions/user';
 
 const initialState = {
   playlists: [],
@@ -23,6 +23,7 @@ export default function playlists(state = initialState, action) {
           name: playlistData.name,
           userId: playlistData.owner.id,
           totalTracks: playlistData.tracks.total,
+          tracks: [],
           isPublic: playlistData.public
         }
       });
@@ -33,6 +34,35 @@ export default function playlists(state = initialState, action) {
         error: null
       });
     case USER_PLAYLIST_CONSTANTS.FAILURE:
+      return Object.assign({}, state, {
+        isFetching: false,
+        error: action.error
+      });
+    case PLAYLIST_TRACKS_CONSTANTS.PENDING:
+      return Object.assign({}, state, {
+        isFetching: true,
+        error: null
+      });
+    case PLAYLIST_TRACKS_CONSTANTS.SUCCESS:
+      let tracks = _.map(action.data.items, (item) => {
+        let track = item.track;
+        return {
+          name: track.name,
+          album: track.album.name,
+          artists: _.map(track.artists, (artist) => artist.name)
+        }
+      });
+      let updatedPlaylists = _.cloneDeep(state.playlists);
+      let index = _.findIndex(updatedPlaylists, (playlist) => playlist.id == action.data.playlistId);
+      console.log('Index: ' + index + 'Playlist id: ' + action.data.playlistId);
+      let playlist = updatedPlaylists[index];
+      playlist.tracks = _.concat(playlist.tracks, tracks);
+      return Object.assign({}, state, {
+        playlists: updatedPlaylists,
+        isFetching: false,
+        error: null
+      });
+    case PLAYLIST_TRACKS_CONSTANTS.FAILURE:
       return Object.assign({}, state, {
         isFetching: false,
         error: action.error
