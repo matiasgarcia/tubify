3148b4302e76a5b7d481b614211b1519b8148fd8
Matias Garcia <garcia.matias92@gmail.com>
2016-09-09 14:10:22 -0300

    Add playlists reducer to store

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import auth from './auth';
+import playlists from './user'
 
 const rootReducer = combineReducers({
   auth,
+  playlists,
   routing,
 });
 
