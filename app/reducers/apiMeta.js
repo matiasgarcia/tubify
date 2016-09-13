bbea1da7e47489aa308881589afbb97110be0b13
matias <mati92.ss@gmail.com>
2016-09-12 22:48:05 -0300

    Add apiMeta for playlist tracks

--- app/reducers/apiMeta.js
+++ app/reducers/apiMeta.js
@@ -1,24 +1,35 @@
-import { USER_PLAYLIST_CONSTANTS } from '../actions/user';
+import _ from 'lodash';
+import { USER_PLAYLIST_CONSTANTS, PLAYLIST_TRACKS_CONSTANTS } from '../actions/user';
+
+function calculateApiPagination(newState, action){
+	let data = action.data;
+  	let offsetMatch = /offset=(\d+)/.exec(data.next);
+  	let nextRequestOffset = offsetMatch ? offsetMatch[1] : 0;
+  	return {
+  		totalCount: data.total,
+  		nextOffset: nextRequestOffset
+  	}
+}
 
 const initialState = {
 	playlists: {
 		totalCount: 0,
 		nextOffset: 0
-	}
-};
+	},
+	playlistTracks: {}
+}
 
 export default function apiMeta(state = initialState, action){
+	let newState = Object.assign({}, state);
 	switch(action.type){
 		case USER_PLAYLIST_CONSTANTS.SUCCESS:
-			let data = action.data;
-  			let offsetMatch = /offset=(\d+)/.exec(data.next);
-  			let nextRequestOffset = offsetMatch ? offsetMatch[1] : 0;
-			return Object.assign({}, state, {
-				playlists: {
-					totalCount: data.total,
-					nextOffset: nextRequestOffset
-				}
+			return _.merge(newState, {
+				playlists: calculateApiPagination(newState, action)
 			});
+		case PLAYLIST_TRACKS_CONSTANTS.SUCCESS:
+			let playlistsTracks = {playlistsTracks: {}};
+			playlistTracks[data.playlistId] = calculateApiPagination(newState, action);
+			return _.merge(newState, playlistTracks)
 		default:
 			return state;
 	}
