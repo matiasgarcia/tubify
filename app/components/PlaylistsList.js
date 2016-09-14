4eafd58a0e468b3e1275e2cc5ecdb1ab7fad4b8b
Matias Garcia <garcia.matias92@gmail.com>
2016-09-14 20:42:26 -0300

    Move PlaylistsList to separate file

---
+++ app/components/PlaylistsList.js
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react';
+import _ from 'lodash';
+import Subheader from 'material-ui/Subheader';
+import Avatar from 'material-ui/Avatar';
+import { ListItem } from 'material-ui/List';
+import SelectableList from './SelectableList'
+
+export default class PlaylistsList extends Component {
+  static propTypes = {
+    playlists: PropTypes.array.isRequired,
+    selected: PropTypes.any,
+    onChange: PropTypes.func.isRequired,
+    total: PropTypes.number
+  };
+  render() {
+    let playlists = this.props.playlists;
+    let pendingCount = this.props.total - playlists.length;
+    return (
+      <SelectableList onChange={this.props.onChange} selected={this.props.selected}>
+        <Subheader>Playlists</Subheader>
+        {_.map(playlists, (playlist) => <ListItem
+          key={playlist.id}
+          value={playlist.id}
+          primaryText={playlist.name}
+          leftAvatar={<Avatar src={playlist.images[0].url}/>}
+        />)}
+        {pendingCount != 0 ? <ListItem key={0} value={0} primaryText={"Load more playlists (" + pendingCount + " left...)"}/> : null}
+      </SelectableList>
+    )
+  }
+}
--- app/components/UserPlaylist.js
+++ app/components/UserPlaylist.js
@@ -1,37 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-import _ from 'lodash';
-import Subheader from 'material-ui/Subheader';
-import Avatar from 'material-ui/Avatar';
 import Paper from 'material-ui/Paper';
-import { ListItem } from 'material-ui/List';
 import { Row, Col } from 'react-flexbox-grid/lib/index';
-import SelectableList from './SelectableList'
-
-class PlaylistsList extends Component {
-  static propTypes = {
-    playlists: PropTypes.array.isRequired,
-    selected: PropTypes.any,
-    onChange: PropTypes.func.isRequired,
-    total: PropTypes.number
-  };
-  render() {
-    let playlists = this.props.playlists;
-    let pendingCount = this.props.total - playlists.length;
-    return (
-      <SelectableList onChange={this.props.onChange} selected={this.props.selected}>
-        <Subheader>Playlists</Subheader>
-        {_.map(playlists, (playlist) => <ListItem
-          key={playlist.id}
-          value={playlist.id}
-          primaryText={playlist.name}
-          leftAvatar={<Avatar src={playlist.images[0].url}/>}
-        />)}
-        {pendingCount != 0 ? <ListItem key={0} value={0} primaryText={"Load more playlists (" + pendingCount + " left...)"}/> : null}
-      </SelectableList>
-    )
-  }
-}
-
+import PlaylistsList from './PlaylistsList'
 
 export default class UserPlaylist extends Component {
   static propTypes = {
